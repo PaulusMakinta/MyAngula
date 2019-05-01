1644671eeb8a84172757c7ab8ffead43cdef9437
Paulus_Makinta <paulusmakinta@icloud.com>
2019-05-01 12:18:21 +0200

    Second Commit

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import { from } from 'rxjs';
+import {Department} from '../models/department.model';
+import { BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,8 +10,29 @@ import { from } from 'rxjs';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  previewphoto = false;
+  datePickerConfig: Partial<BsDatepickerConfig>;
+  departments: Department[] =[
+   
+    {id: 1, name: "HelpDesk"},
+    { id: 2,name: "Human Resource"},
+    { id: 3,name: "Accounting"},
+    {id: 4 ,name: "IT"} ,
+    {id: 5 ,name: "select_Department"}
+  ] ;
+  department = "5"
+  constructor() { 
+    this.datePickerConfig = Object.assign({},
+      {
+        containerClass: 'theme-dark-blue',
+        showWeekNumbers: false,
+        dateInputFormat: 'DD/MM/YYYY'
+      });
+  }
 
-  constructor() { }
+  togglePhotoPreview(){
+   this.previewphoto = !this.previewphoto;
+  }
 
   ngOnInit() {
   }
